1b2b14b42b85dcf6d160b6c0aa3eefe63cd05e2f
bradkillins <bradkillins@gmail.com>
2020-02-25 00:37:45 -0700

    just some final touches left for filter and sort

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
  * Shared Workspace Web App
  * SODV1201
  * Brad Killins
+ *
+ * Phase 1
+ *
  */
 
 // User constructor
@@ -40,16 +43,13 @@ const SaveUser = user => {
   //check if users array exists in localStorage
   if (!localStorage.getItem("users")) {
     //if no users array exist:
-    console.log("Created new users array in localStorage"); //for testing
     let users = []; //create the users array
     users.push(user); //add newUser to users array
     //save users array to localStorage as 'users'
     //using JSON stringify as anything saved in local storage is converted to a string
     localStorage.setItem("users", JSON.stringify(users));
   } else {
-    //if users array already exists
-    console.log("Add to users array in localStorage");
-    //get users array from storage
+    //if users array already exists get users array from storage
     //parse from string back to array using JSON.parse
     let users = JSON.parse(localStorage.getItem("users"));
     users.push(user);
@@ -61,7 +61,6 @@ const SaveProperty = property => {
   //check properties array exists in localStorage
   if (!localStorage.getItem("properties")) {
     //if no properties array exist:
-    console.log("Created new properties array in localStorage"); //for testing
     let properties = []; //create the properties array
     properties.push(property); //add newProperty to properties array
     //save properties array to localStorage as 'properties'
@@ -114,16 +113,14 @@ const CreateNewUser = () => {
 
     document.querySelector(".formFeedback").innerHTML =
       "Successfully created user! <br/> Re-directing to login page in 5 seconds...";
-    document.querySelector("#signup").reset();
-    /*
-  setTimeout(() => {
-    document.location = "./login.html";
-  }, 5000); */
   } else {
     document.querySelector(".formFeedback").innerHTML =
       "User with same email already exists! Please try logging in. <br/> Re-directing to login page in 5 seconds...";
-    document.querySelector("#signup").reset();
   }
+  document.querySelector("#signup").reset();
+  setTimeout(() => {
+    document.location = "./login.html";
+  }, 5000);
 };
 
 //Create new Property
@@ -415,15 +412,29 @@ const ShowWorkspaces = () => {
 
   const workspacesTable = document.querySelector("#workspacesTable");
   currentProp.workspace.forEach(e => {
+    let type;
+    if (e.type === "meet") type = "Meeting Room";
+    if (e.type === "office") type = "Private Office";
+    if (e.type === "desk") type = "Open Area Desk";
+
+    let smoke = e.smoke ? "Yes" : "No";
+
+    let term;
+    if (e.term === "day") term = "1 Day";
+    if (e.term === "week") term = "1 Week";
+    if (e.term === "month") term = "1 Month";
+
+    let listed = e.listed ? "Yes" : "No";
+
     let result = `<tbody><tr>
       <td><button name="${e.id}" id="${currentProp.id}" onclick="EditSelectedWorkspace(this.id, this.name);">Edit</button></td>
-      <td>${e.type}</td>
+      <td>${type}</td>
       <td>${e.occ}</td>
-      <td>${e.smoke}</td>
+      <td>${smoke}</td>
       <td>${e.availDate}</td>
-      <td>${e.term}</td>
+      <td>${term}</td>
       <td>${e.price}</td>
-      <td>${e.listed}</td>
+      <td>${listed}</td>
       </tr></tbody>`;
     workspacesTable.insertAdjacentHTML("beforeend", result);
   });
@@ -544,28 +555,21 @@ const DeleteCurrentWork = () => {
   );
 
   properties[propIndex].workspace.splice(workIndex, 1);
+  localStorage.setItem("properties", JSON.stringify(properties));
+  sessionStorage.setItem("currentProp", JSON.stringify(properties[propIndex]));
+  sessionStorage.setItem("currentWork", JSON.stringify([]));
+
+  document.location = "./ownerWorkspace.html";
 };
 
-const FormListener = (selector, func) => {
-  //Only add the event listener if the selector exists on the current page.
-  if (document.querySelector(selector))
-    document.querySelector(selector).addEventListener(
-      "submit",
-      event => {
-        event.preventDefault(); //Stops the page from refreshing when submitting the form.
-        func(); //The function to run when the form is submitted.
-      } /*,
-      false*/
-    );
-};
-
-//displays the current user name in the menu bar when logged in
+//displays the current user name in #displayUserName
 const DisplayUserName = () => {
   const displayName = document.querySelector("#displayUserName");
-  let name = JSON.parse(sessionStorage.getItem("currentUser")).fName;
+  const name = JSON.parse(sessionStorage.getItem("currentUser")).fName;
   displayName.innerText = name;
 };
 
+//displays the current property address in #titleProp
 const DisplayPropName = () => {
   const displayTitle = document.querySelector("#titleProp");
   const title = JSON.parse(sessionStorage.getItem("currentProp")).address;
@@ -583,7 +587,292 @@ const GenRanId = length => {
   return id;
 };
 
-//**** Main execution starts here ****
+//SaveAvailWorkspaces - Builds an array of all available workspaces
+// and saves it as "availWorkspaces" in sessionStorage.
+const SaveAvailWorkspaces = () => {
+  //retrieve properties
+  const properties = JSON.parse(localStorage.getItem("properties"));
+  let availProps = properties.filter(e => e.listed);
+  let availWork = [];
+  availProps.forEach(prop => {
+    prop.workspace.forEach(work => {
+      if (work.listed) {
+        work.address = prop.address;
+        work.neighbor = prop.neighbor;
+        work.sqFeet = prop.sqFeet;
+        work.parking = prop.parking;
+        work.transit = prop.transit;
+        work.owner = prop.owner;
+        availWork.push(work);
+      }
+    });
+  });
+  sessionStorage.setItem("availWorkspaces", JSON.stringify(availWork));
+};
+
+//sessionStorage: availWorkspaces  -- the master array of available workspaces - DO NOT MODIFY
+//                displayWorkspaces -- current
+
+const PopulateWorkTable = dataset => {
+  //load the dataset from sessionStorage
+  const workspaces = JSON.parse(sessionStorage.getItem(dataset));
+  if (!workspaces.length) {
+    console.log(document.querySelector("#noWorkspaces"));
+    document.querySelector(
+      "#noWorkspaces"
+    ).innerHTML = `Sorry there are no available workspaces. Try changing the filter settings. 
+      <br/> There also may not be any Workspaces available.`;
+  }
+
+  const table = document.querySelector("#availWorkTable");
+  let body =
+    //table headers
+    `<tbody>
+        <tr>
+            <th>
+                Address <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Neighborhood <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Type <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Occupancy <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Sq Feet <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Term <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Price <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Available Date <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Parking Garage <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Smoking <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+            <th>
+                Transit <button onclick="">&uarr;</button><button onclick="">&darr;</button>
+            </th>
+        </tr>
+    </tbody>`;
+
+  workspaces.forEach(workspace => {
+    const address = workspace.address;
+    const neighbor = workspace.neighbor;
+    let type = workspace.type;
+    const occ = workspace.occ;
+    const sqFeet = workspace.sqFeet;
+    let term = workspace.term;
+    const price = workspace.price;
+    const availDate = workspace.availDate;
+    const parking = workspace.parking;
+    const smoke = workspace.smoke;
+    const transit = workspace.transit;
+
+    if (type === "meet") type = "Meeting Room";
+    if (type === "office") type = "Private Office";
+    if (type === "desk") type = "Open Area Desk";
+
+    if (term === "day") term = "1 Day";
+    if (term === "week") term = "1 Week";
+    if (term === "month") term = "1 Month";
+
+    body += `<tbody>
+                <tr>
+                    <td>${address}</td>
+                    <td>${neighbor}</td>
+                    <td>${type}</td>
+                    <td>${occ}</td>
+                    <td>${sqFeet}</td>
+                    <td>${term}</td>
+                    <td>${price}</td>
+                    <td>${availDate}</td>
+                    <td>${parking ? "Yes" : "No"}</td>
+                    <td>${smoke ? "Yes" : "No"}</td>
+                    <td>${transit ? "Yes" : "No"}</td>
+                </tr>
+            </tbody>`;
+  });
+  table.innerHTML = body;
+};
+
+const firstLoadWorkTable = () => {
+  //
+  SaveAvailWorkspaces();
+  PopulateWorkTable("availWorkspaces");
+};
+
+const InsertFilterInput = () => {
+  const filSelect = document.querySelector("#filterBy");
+  const insertPoint = document.querySelector("#filterTerm");
+  let insertInput;
+  if (filSelect.options[filSelect.selectedIndex].value === "none") {
+    insertInput = `<label for="filterTerm">Filter Term: </label>
+        <input type="text" id="filterTerm" disabled />`;
+  }
+
+  if (
+    filSelect.options[filSelect.selectedIndex].value === "address" ||
+    filSelect.options[filSelect.selectedIndex].value === "neighbor"
+  ) {
+    insertInput = `<label for="filterTerm">Filter Term: </label>
+            <input type="text" id="filterTerm" required maxlength="100" />`;
+  }
+
+  if (filSelect.options[filSelect.selectedIndex].value === "type") {
+    insertInput = `<label for="filterTerm">Filter Type: </label>
+      <select name="filterTerm" id="filterTerm">
+        <option value="meet">Meeting Room</option>
+        <option value="office">Private Office</option>
+        <option value="desk">Open Area Desk</option>
+      </select>`;
+  }
+
+  if (
+    filSelect.options[filSelect.selectedIndex].value === "occ" ||
+    filSelect.options[filSelect.selectedIndex].value === "sqFeet"
+  ) {
+    insertInput = `<label for="filterTerm">Filter Term: </label>
+        <input type="number" id="filterTerm" required min="1" />`;
+  }
+
+  if (filSelect.options[filSelect.selectedIndex].value === "term") {
+    insertInput = `<label for="filterTerm">Term: </label>
+      <select name="filterTerm" id="filterTerm">
+        <option value="day">1 Day</option>
+        <option value="week">1 Week</option>
+        <option value="month">1 Month</option>
+      </select>`;
+  }
+
+  if (filSelect.options[filSelect.selectedIndex].value === "availDate") {
+    insertInput = `<label for="filterTerm">This Date: </label>
+      <input type="date" id="filterTerm" required />`;
+  }
+
+  if (
+    filSelect.options[filSelect.selectedIndex].value === "parking" ||
+    filSelect.options[filSelect.selectedIndex].value === "smoke" ||
+    filSelect.options[filSelect.selectedIndex].value === "transit"
+  ) {
+    insertInput = `Select:
+    <label for="filterTermYes"
+      >Yes
+      <input
+        type="radio"
+        class="radio"
+        id="filterTermYes"
+        name="filterTerm"
+        checked
+    /></label>
+    <label for="filterTermNo"
+      >No 
+      <input 
+        type="radio" 
+        class="radio" 
+        id="filterTermNo" 
+        name="filterTerm"
+    /></label>`;
+  }
+
+  insertPoint.innerHTML = insertInput;
+};
+
+const FilterReady = () => {
+  //get filterBy and filterTerm from DOM
+  const filSelect = document.querySelector("#filterBy");
+  const filterBy = filSelect.options[filSelect.selectedIndex].value;
+
+  let filterInput;
+  //input box only filterBy options
+  if (
+    filterBy === "address" ||
+    filterBy === "neighbor" ||
+    filterBy === "occ" ||
+    filterBy === "sqFeet" ||
+    filterBy === "availDate"
+  ) {
+    filterInput = document.querySelector("#filterTerm").value;
+  }
+  //select list filterBy options
+  if (filterBy === "type" || filterBy === "term") {
+    //     const
+  }
+
+  //run the filter
+  Filter(filterBy, filterInput);
+};
+
+const Filter = (filterBy, filterInput) => {
+  //retrieve availWorkspaces from sessionStorage
+  const originalData = JSON.parse(sessionStorage.getItem("availWorkspaces"));
+
+  let filteredData;
+  if (filterBy === "address") {
+    filteredData = originalData.filter(e => e.address.includes(filterInput));
+  }
+  if (filterBy === "neighbor") {
+    filteredData = originalData.filter(e => e.neighbor.includes(filterInput));
+  }
+  if (filterBy === "type") {
+    filteredData = originalData.filter(e => e.type.includes(filterInput));
+  }
+  if (filterBy === "occ") {
+    filteredData = originalData.filter(e => e.occ.includes(filterInput));
+  }
+  if (filterBy === "sqFeet") {
+    filteredData = originalData.filter(e => e.sqFeet.includes(filterInput));
+  }
+  if (filterBy === "term") {
+    filteredData = originalData.filter(e => e.term.includes(filterInput));
+  }
+  if (filterBy === "availDate") {
+    filteredData = originalData.filter(e => e.availDate.includes(filterInput));
+  }
+  if (filterBy === "parking") {
+    filteredData = originalData.filter(e => e.parking.includes(filterInput));
+  }
+  if (filterBy === "smoke") {
+    filteredData = originalData.filter(e => e.smoke.includes(filterInput));
+  }
+  if (filterBy === "transit") {
+    filteredData = originalData.filter(e => e.transit.includes(filterInput));
+  }
+  if (filterBy === "none") {
+    filteredData = originalData;
+  }
+
+  //store filtered data in displayWorkspaces
+  sessionStorage.setItem("displayWorkspaces", JSON.stringify(filteredData));
+  //display the filtered data
+  PopulateWorkTable("displayWorkspaces");
+};
+
+//Sort(dataset)
+
+//adds an event listener listening for submit, then blocks the page from refreshing
+// and runs a function.
+const FormListener = (selector, func) => {
+  //Only add the event listener if the selector exists on the current page.
+  if (document.querySelector(selector))
+    document.querySelector(selector).addEventListener(
+      "submit",
+      event => {
+        event.preventDefault(); //Stops the page from refreshing when submitting the form.
+        func(); //The function to run when the form is submitted.
+      } /*,
+        false*/
+    );
+};
 
 FormListener("#signup", CreateNewUser);
 FormListener("#login", Login);
@@ -591,3 +880,4 @@ FormListener("#addProperty", CreateNewProperty);
 FormListener("#editProperty", EditCurrentProp);
 FormListener("#addWorkspace", CreateNewWorkspace);
 FormListener("#editWorkspace", EditCurrentWork);
+FormListener("#filterForm", Filter);
